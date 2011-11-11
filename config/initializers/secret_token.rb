004c4cf655c0cd15348d2aeb420cc0793095664b
jelmer <jelmer@jelmer-VirtualBox.(none)>
2011-11-11 12:32:15 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvapp::Application.config.secret_token = 'aa96469d94933fe49848dfe24ecc311aee3953e1388e757fb838fa45b5a1d47d536c0fbacae2411536397d8d0cd21073e831a95f68b11ad06aaa7e17923a5f4b'
